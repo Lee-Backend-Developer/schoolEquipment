6069a4ed1781638b324820930a4e9c0727fb9d28
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-04-22 18:32:05 +0900

    장비 및 수업시간 장비 대여 기능 구현 완료, 테스트코드 작성

--- src/main/java/com/equipment/school_equipment/service/RentalService.java
+++ src/main/java/com/equipment/school_equipment/service/RentalService.java
@@ -1,12 +1,28 @@
 package com.equipment.school_equipment.service;
 
+import com.equipment.school_equipment.domain.ClassTime;
+import com.equipment.school_equipment.domain.Equipment;
+import com.equipment.school_equipment.domain.Rental;
+import com.equipment.school_equipment.repository.ClassTimeRepository;
 import com.equipment.school_equipment.repository.EquipmentRepository;
+import com.equipment.school_equipment.repository.RentalRepository;
+import com.equipment.school_equipment.request.rental.RentalCreate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RentalService {
 
     private final EquipmentRepository equipmentRepository;
+    private final ClassTimeRepository classTimeRepository;
+    private final RentalRepository rentalRepository;
+
+    public Rental rentalCreate(RentalCreate request) {
+        Rental rental = new Rental(request.classTime(), request.equipment());
+        return rentalRepository.save(rental);
+    }
 }
--- src/test/java/com/equipment/school_equipment/service/RentalServiceTest.java
+++ src/test/java/com/equipment/school_equipment/service/RentalServiceTest.java
@@ -1,45 +1,61 @@
 package com.equipment.school_equipment.service;
 
+import com.equipment.school_equipment.domain.ClassTime;
+import com.equipment.school_equipment.domain.Equipment;
 import com.equipment.school_equipment.repository.ClassTimeRepository;
 import com.equipment.school_equipment.repository.EquipmentRepository;
+import com.equipment.school_equipment.repository.RentalRepository;
+import com.equipment.school_equipment.request.rental.RentalCreate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 public class RentalServiceTest {
     @Autowired
     private ClassTimeRepository classTimeRepository;
     @Autowired
     private EquipmentRepository equipmentRepository;
     @Autowired
+    private RentalRepository rentalRepository;
+    @Autowired
     private RentalService rentalService;
+
+    // 장비
+    private Equipment equipment;
+    // 시간
+    private ClassTime classTime;
 
     @BeforeEach
     void setUp() {
-
+        equipment = equipmentRepository.save(new Equipment("pmw", 10));
+        classTime = classTimeRepository.save(ClassTime.builder().className("영상촬영실습").twoTime(true).threeTime(true).fourTime(true).build());
     }
 
     @AfterEach
     void end() {
-
+        equipmentRepository.deleteAll();
+        classTimeRepository.deleteAll();
+        rentalRepository.deleteAll();
     }
 
     @DisplayName("장비를 대여가 되고 장비도 대어한 만큼 줄어들어야함")
     @Test
     void equipmentRental() {
         //given
-//        new RentalCreateDto(new ClassTime(null), new Equipment(null, null));
+        RentalCreate request = new RentalCreate(classTime, equipment);
 
         //when
-//        rentalService.rentalCreate(RentalCreateDto dto);
+        rentalService.rentalCreate(request);
 
         //then
-        assertThat(equipmentRepository.count()).isEqualTo(0);
+        assertThat(equipmentRepository.count()).isEqualTo(1);
     }
 }
