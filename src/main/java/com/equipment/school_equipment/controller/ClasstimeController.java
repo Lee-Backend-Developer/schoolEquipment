7b86f5c24d76cfea7254d591f1461bc473440544
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-04-24 00:01:14 +0900

    수업시간표 페이지
    No에 요일별로 출력되게 하였음

--- src/main/java/com/equipment/school_equipment/controller/ClasstimeController.java
+++ src/main/java/com/equipment/school_equipment/controller/ClasstimeController.java
@@ -1,16 +1,25 @@
 package com.equipment.school_equipment.controller;
 
+import com.equipment.school_equipment.domain.enumDomain.DayOfWeekEnum;
+import com.equipment.school_equipment.service.ClassTimeService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class ClasstimeController {
+    private final ClassTimeService classTimeService;
 
     @RequestMapping("/classtimes")
-    public String classtimes() {
+    public String classtimes(Model model) {
+        model.addAttribute(DayOfWeekEnum.values());
         return "classTimeList";
     }
 }
