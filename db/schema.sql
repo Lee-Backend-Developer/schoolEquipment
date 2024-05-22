705aa19dd95d4181fbaf7368624d337593903ecf
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-05-22 20:11:13 +0900

    SQL 쿼리 수정

--- db/schema.sql
+++ db/schema.sql
@@ -1,65 +1,65 @@
 create table category
 (
-    category_id bigint auto_increment,
-    category_name         varchar(255) not null unique,
+    id            bigint auto_increment,
+    category_name varchar(255) not null unique,
 
-    primary key (category_id)
+    primary key (id)
 ) comment '물품 카테고리 테이블';
 
 create table equipment
 (
-    equipment_id      bigint auto_increment,
-    name              varchar(40)  not null unique,
-    category_id bigint       not null,
-    main_img          varchar(100) null,
-    content           text null,
-    count             integer      not null,
+    id bigint auto_increment,
+    name         varchar(40)  not null unique,
+    category_id  bigint       not null,
+    main_img     varchar(100) null,
+    content      text         null,
+    count        integer      not null,
 
-    foreign key (category_id) references category (category_id),
-    primary key (equipment_id)
+    foreign key (category_id) references category (id),
+    primary key (id)
 ) comment '물품 테이블';
 
-create table classes
+create table class_period
 (
-    classes_id         bigint auto_increment,
-    class_name         varchar(255)                                              not null,
-    day_of_week        enum ('monday','tuesday','wednesday','thursday','friday') not null,
-    one_time           boolean default false,
-    two_time           boolean default false,
-    three_time         boolean default false,
-    four_time          boolean default false,
-    five_time          boolean default false,
-    six_time           boolean default false,
-    seven_time         boolean default false,
-    eight_time         boolean default false,
-    nine_time          boolean default false,
-    ten_time           boolean default false,
+    id  bigint auto_increment,
+    class_name  varchar(255)                                              not null,
+    day_of_week enum ('monday','tuesday','wednesday','thursday','friday') not null,
+    one_time    boolean default false,
+    two_time    boolean default false,
+    three_time  boolean default false,
+    four_time   boolean default false,
+    five_time   boolean default false,
+    six_time    boolean default false,
+    seven_time  boolean default false,
+    eight_time  boolean default false,
+    nine_time   boolean default false,
+    ten_time    boolean default false,
 
-    primary key (classes_id)
+    primary key (id)
 ) comment '수업명, 1~10 교시 테이블';
 
 
 create table rental
 (
-    rental_id          bigint auto_increment comment '대여 id',
-    classtimelist_id bigint not null comment '수업 시간 테이블의 외래키',
-    equipment_id       bigint not null comment '물품 테이블의 외래키',
-    rental_chk         boolean default true comment '대여 가능 여부',
-    rental_cnt         int     default 0 comment '대여한 수량',
+    id        bigint auto_increment comment '대여 id',
+    class_period_id bigint not null comment '수업 시간 테이블의 외래키',
+    equipment_id     bigint not null comment '물품 테이블의 외래키',
+    rental_chk       boolean default true comment '대여 가능 여부',
+    rental_cnt       int     default 0 comment '대여한 수량',
 
-    foreign key (classtimelist_id) references classes (classtimelist_id),
-    foreign key (equipment_id) references equipment (equipment_id),
-    primary key (rental_id)
+    foreign key (class_period_id) references class_period (id),
+    foreign key (equipment_id) references equipment (id),
+    primary key (id)
 ) comment '수업 시간에 대여한 물품';
 
 # rental 테이블에서 오늘이 월요일이면 월요일에 대한 대여목록 가져오기
 # 뷰로 만들어도 되지만 혹시 학생이 볼수있게 하게 되면 수량를 가릴수 있어야함
 create table today_rental
 (
-    today_id bigint auto_increment comment 'today table에 id',
-    rental_id      bigint not null comment 'rental table의 외래키',
-    minus_count     int    not null  comment '대여한 수량',
+    id    bigint auto_increment comment 'today table에 id',
+    rental_id   bigint not null comment 'rental table의 외래키',
+    rental_minus_count int    not null comment '대여한 수량',
 
-    foreign key (rental_id) references rental (rental_id),
-    primary key (today_id)
+    foreign key (rental_id) references rental (id),
+    primary key (id)
 ) COMMENT '오늘 대여 가능한 물품';
