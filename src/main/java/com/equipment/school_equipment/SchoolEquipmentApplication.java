1262c43b196e6839de4f0b68bb871eb9ea7c0159
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-05 23:01:41 +0900

    DB 운영서버 정보 숨김 및 설정

--- src/main/java/com/equipment/school_equipment/SchoolEquipmentApplication.java
+++ src/main/java/com/equipment/school_equipment/SchoolEquipmentApplication.java
@@ -2,10 +2,12 @@ package com.equipment.school_equipment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource("classpath:/aws.properties")
 public class SchoolEquipmentApplication {
 
     public static void main(String[] args) {
