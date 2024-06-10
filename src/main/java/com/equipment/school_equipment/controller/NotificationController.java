9780935ec979c2dcb7028ee51f4dcd6e0366ec47
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-11 00:04:21 +0900

    공지사항 메인사진, 유의사항 클릭시 화면 뜰수있게 SPA 구현함

---
+++ src/main/java/com/equipment/school_equipment/controller/NotificationController.java
@@ -0,0 +1,19 @@
+package com.equipment.school_equipment.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/admin/notification")
+public class NotificationController {
+
+    @GetMapping("product")
+    public String getProduct(){
+        return "admin/notification/edit";
+    }
+}
