0f373306f0344929c852652f7adadcd5d5a3f43e
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-11 23:04:44 +0900

    컨트롤러: 공지 아이디를 통해서 조회하는 기능구현 완료

--- src/main/java/com/equipment/school_equipment/controller/admin/api/NotificationProductApiController.java
+++ src/main/java/com/equipment/school_equipment/controller/admin/api/NotificationProductApiController.java
@@ -1,11 +1,13 @@
 package com.equipment.school_equipment.controller.admin.api;
 
+import com.equipment.school_equipment.domain.NotificationProduct;
 import com.equipment.school_equipment.response.api.NotificationProductResponse;
 import com.equipment.school_equipment.service.NotificationProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,19 @@ import java.util.List;
 public class NotificationProductApiController {
 
     private final NotificationProductService notificationProductService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NotificationProductResponse> getNotification(@PathVariable Long id){
+        NotificationProduct findNotification = notificationProductService.findById(id);
+        NotificationProductResponse response = NotificationProductResponse.builder()
+                .id(findNotification.getId())
+                .img(findNotification.getImg())
+                .subject(findNotification.getSubject())
+                .content(findNotification.getContent())
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
 
     @GetMapping
     public ResponseEntity<List<NotificationProductResponse>> getNotification(){
