ec4b399fb0585cf9be4e3aa0a51a449d10924398
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-12 17:16:18 +0900

    메인공지사항 조회 기능 구현 완료

--- src/main/java/com/equipment/school_equipment/controller/MainController.java
+++ src/main/java/com/equipment/school_equipment/controller/MainController.java
@@ -1,15 +1,21 @@
 package com.equipment.school_equipment.controller;
 
+import com.equipment.school_equipment.service.NotificationProductService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class MainController {
+    private final NotificationProductService notificationProductService;
 
     @GetMapping("/")
-    public String MainHomePage() {
+    public String MainHomePage(Model model) {
+        model.addAttribute("findList", notificationProductService.findList());
         return "mainHome";
     }
 }
