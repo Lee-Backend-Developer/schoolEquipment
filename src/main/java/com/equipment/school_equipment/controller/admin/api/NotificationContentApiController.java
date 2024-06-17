8c554c948ef8c4ecb6c6876c404233b25fe5af54
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-17 13:57:50 +0900

    공지사항 조회 기능 구현

--- src/main/java/com/equipment/school_equipment/controller/admin/api/NotificationContentApiController.java
+++ src/main/java/com/equipment/school_equipment/controller/admin/api/NotificationContentApiController.java
@@ -1,15 +1,13 @@
 package com.equipment.school_equipment.controller.admin.api;
 
+import com.equipment.school_equipment.domain.NotificationContent;
 import com.equipment.school_equipment.repository.NotificationContentRepository;
 import com.equipment.school_equipment.request.notificationProduct.NotificationRequest;
 import com.equipment.school_equipment.service.NotificationContentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/admin/notification/content")
 public class NotificationContentApiController {
     private final NotificationContentService notificationContentService;
+
+    @GetMapping
+    public NotificationContent contentFind() {
+        NotificationContent finds = notificationContentService.finds();
+        return finds;
+    }
 
     @PostMapping
     public ResponseEntity contentSave(NotificationRequest notificationRequest) {
