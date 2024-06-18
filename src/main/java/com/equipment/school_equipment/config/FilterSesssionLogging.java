49cf358a241081aebec1cd427eaf0826ef782806
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-18 17:44:40 +0900

    로그인 기능구현 완료

---
+++ src/main/java/com/equipment/school_equipment/config/FilterSesssionLogging.java
@@ -0,0 +1,32 @@
+package com.equipment.school_equipment.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+//@Component
+public class FilterSesssionLogging implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(FilterSesssionLogging.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        log.info("session => {}", request.getSession().getId());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
--- src/main/java/com/equipment/school_equipment/controller/LoginController.java
+++ src/main/java/com/equipment/school_equipment/controller/LoginController.java
@@ -6,11 +6,15 @@ import com.equipment.school_equipment.request.admin.UserRequest;
 import com.equipment.school_equipment.service.LoginUserService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import java.util.Objects;
 
 @Controller
 @Slf4j
@@ -19,24 +23,32 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
 
     private final LoginUserService loginUserService;
-    
+    private final HttpSession httpSession;
+
     @GetMapping("login")
     public String getLogin(@ModelAttribute(binding = false, name = "userRequest") UserRequest userRequest) {
-        return "member/login";
+
+        return Objects.isNull(httpSession.getAttribute("clientSession")) ? "member/login" : "redirect:/";
     }
 
     @PostMapping("login")
     public String postLogin(
-            @Valid UserRequest userRequest, BindingResult bindingResult, HttpSession httpSession){
-        if(bindingResult.hasErrors()) {
+            @Valid UserRequest userRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "member/login";
         }
         LoginUser login = loginUserService.login(userRequest);
-        httpSession.setAttribute("client", login);
+        httpSession.setMaxInactiveInterval(0);
+        httpSession.setAttribute("clientSession", login);
 
         return "redirect:/";
     }
 
+    @GetMapping("logout")
+    public String getLogout() {
+        httpSession.invalidate();
+        return "redirect:/";
+    }
 
 
 }
