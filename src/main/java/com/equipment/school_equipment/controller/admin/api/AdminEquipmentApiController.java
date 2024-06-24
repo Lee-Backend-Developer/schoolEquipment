d944613904aa7ef8be3a01ec740ee5bcf55f00bd
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-24 19:20:43 +0900

    대여추가 바닐라JS 구현

--- src/main/java/com/equipment/school_equipment/controller/admin/api/AdminEquipmentApiController.java
+++ src/main/java/com/equipment/school_equipment/controller/admin/api/AdminEquipmentApiController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/admin/equipment/api")
+@RequestMapping("api/admin/equipment")
 public class AdminEquipmentApiController {
     private final EquipmentService equipmentService;
 
