176636d1f7c5b56b0e52b89cd32e1f5b4da0159a
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-06-26 21:49:51 +0900

    로그인 컨트롤러 세션객체 삭제, 로그인 GET메소드만 있음

--- src/main/java/com/equipment/school_equipment/controller/LoginController.java
+++ src/main/java/com/equipment/school_equipment/controller/LoginController.java
@@ -1,23 +1,12 @@
 package com.equipment.school_equipment.controller;
 
-import com.equipment.school_equipment.controller.session.SessionObj;
-import com.equipment.school_equipment.domain.LoginUser;
 import com.equipment.school_equipment.request.admin.UserRequest;
-import com.equipment.school_equipment.service.LoginUserService;
-import jakarta.security.auth.message.AuthException;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Objects;
 
 @Controller
 @Slf4j
@@ -27,15 +16,7 @@ public class LoginController {
 
     @GetMapping("login")
     public String getLogin(@ModelAttribute(binding = false, name = "userRequest") UserRequest userRequest) {
-
         return "member/login";
     }
-
-  /*  @GetMapping("logout")
-    public String getLogout() {
-        httpSession.invalidate();
-        return "redirect:/";
-    }*/
-
 
 }
