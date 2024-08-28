66ffab011dd3ef0a79a5b08a0418a18387300f9b
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-08-28 09:49:26 +0900

    KAN-19 기능수정: 오늘 대여된 장비중에서 만약에 없을경우 예외처리를 했는데 메시지를 간략하게 "기자재 대여 없습니다" 출력

--- src/main/java/com/equipment/school_equipment/service/TodayRentalService.java
+++ src/main/java/com/equipment/school_equipment/service/TodayRentalService.java
@@ -6,11 +6,13 @@ import com.equipment.school_equipment.domain.enumDomain.DayOfWeekEnum;
 import com.equipment.school_equipment.repository.RentalRepository;
 import com.equipment.school_equipment.repository.TodayRentalRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -23,7 +25,12 @@ public class TodayRentalService {
     public void createRentalFromToday(DayOfWeekEnum weekday) {
         todayRentalRepository.deleteAll();
         List<Rental> rentalList = rentalRepository.findByWeekday(weekday);
-        if(rentalList.isEmpty()) throw new NullPointerException("대여된 장비가 없습니다");
+        try {
+            if(rentalList.isEmpty())
+                throw new NullPointerException();
+        } catch (NullPointerException e) {
+            log.error("대여된 장비가 없습니다.");
+        }
         rentalList.forEach(rental -> todayRentalRepository.save(TodayRental.builder()
                 .rentalMinusCount(rental.getRentalCnt())
                 .rental(rental)
