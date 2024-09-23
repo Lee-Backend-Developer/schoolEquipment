4f1c078422afb7f0c2d1f52467581cb823d729d1
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-09-23 14:24:35 +0900

    모키토를 적용해서 LoginMemberConnectLog 테스트코드 짜봤음

--- src/test/java/com/equipment/school_equipment/service/LoginMemberConnectLogServiceTest.java
+++ src/test/java/com/equipment/school_equipment/service/LoginMemberConnectLogServiceTest.java
@@ -3,38 +3,53 @@ package com.equipment.school_equipment.service;
 import com.equipment.school_equipment.domain.LoginUserConnectLog;
 import com.equipment.school_equipment.domain.member.Member;
 import com.equipment.school_equipment.repository.LoginUserConnectLogRepository;
-import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Timestamp;
+import java.util.List;
 
-@Transactional
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 class LoginMemberConnectLogServiceTest {
-    @Autowired
+    @Mock
     private LoginUserConnectLogRepository loginUserConnectLogRepository;
-    @Autowired
+    @InjectMocks
     private LoginUserConnectLogService loginUserConnectLogService;
+
 
     @DisplayName("접속한 A 계정의 접속 로그 IP를 조회함")
     @Test
     void find_O() throws Exception {
         //given 준비
-        Member memberA = Member.builder()
+
+        Member memberA = Member.builder() // spy
+                .id(1L)
                 .userId("A")
                 .build();
-        LoginUserConnectLog saveLoginUserConnectLog = LoginUserConnectLog.builder()
+
+        LoginUserConnectLog saveLoginUserConnectLog = LoginUserConnectLog.builder() // spy
                 .memberKey(memberA)
                 .connectIp("127.0.0.1")
                 .connectDate(new Timestamp(System.currentTimeMillis()))
                 .build();
-        loginUserConnectLogRepository.save(saveLoginUserConnectLog);
+
+        // stubbing
+        given(loginUserConnectLogRepository.findByMemberKeyId(any(Long.class)))
+                .willReturn(List.of(saveLoginUserConnectLog));
+
         //when 실행
+        List<LoginUserConnectLog> findById = loginUserConnectLogService.findById(memberA.getId());
 
         //then 검증
+        Assertions.assertThat(findById.size()).isEqualTo(1);
 
     }
 
